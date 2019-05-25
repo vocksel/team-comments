529a5dac7f7609b6fd6426769b48ea05395c2232
David Minnerly <voxeldavid@gmail.com>
2019-05-25 11:45:34 -0400

    Setup message visibility toggling

--- src/Components/WorldMessages.lua
+++ src/Components/WorldMessages.lua
@@ -3,20 +3,23 @@ local Connect = require(script.Parent.Parent.Lib.RoactRodux).connect
 local MessageBillboard = require(script.Parent.MessageBillboard)
 
 local function WorldMessages(props)
-	local children = {}
+	if props.ui.areMessagesVisible then
+		local children = {}
 
-	for _, message in pairs(props.messages) do
-		children[message.id] = Roact.createElement(MessageBillboard, {
-			message = message
-		})
+		for _, message in pairs(props.messages) do
+			children[message.id] = Roact.createElement(MessageBillboard, {
+				message = message
+			})
+		end
+
+		return Roact.createElement("Folder", {}, children)
 	end
-
-	return Roact.createElement("Folder", {}, children)
 end
 
 local function mapStateToProps(state)
 	return {
-		messages = state.messages
+		messages = state.messages,
+		ui = state.ui,
 	}
 end
 

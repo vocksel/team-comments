3edac971825e2d4d59b1546f2453c3a4ba51ba04
David Minnerly <voxeldavid@gmail.com>
2019-05-25 11:22:01 -0400

    Scale the billboards based on distance

---
+++ src/Components/CameraDistanceProvider.lua
@@ -0,0 +1,38 @@
+local RunService = game:GetService("RunService")
+
+local Roact = require(script.Parent.Parent.Lib.Roact)
+local t = require(script.Parent.Parent.Lib.t)
+
+local camera = workspace.CurrentCamera
+
+local Props = t.interface({
+	origin = t.Vector3,
+	render = t.callback
+})
+
+local CameraDistanceProvider = Roact.Component:extend("CameraDistanceProvider")
+
+function CameraDistanceProvider:init()
+	self.state = {
+		distance = 0
+	}
+end
+
+function CameraDistanceProvider:render()
+	assert(Props(self.props))
+	return self.props.render(self.state.distance)
+end
+
+function CameraDistanceProvider:didMount()
+	self.connection = RunService.RenderStepped:Connect(function()
+		print("working")
+		local distance = (self.props.origin-camera.CFrame.p).Magnitude
+		self:setState({ distance = distance })
+	end)
+end
+
+function CameraDistanceProvider:willUnmount()
+	self.connection:Disconnect()
+end
+
+return CameraDistanceProvider
--- src/Components/MessageBillboard.lua
+++ src/Components/MessageBillboard.lua
@@ -7,6 +7,7 @@ local Avatar = require(script.Parent.Avatar)
 local MessageMeta = require(script.Parent.MessageMeta)
 local MessageBody = require(script.Parent.MessageBody)
 local Padding = require(script.Parent.Padding)
+local CameraDistanceProvider = require(script.Parent.CameraDistanceProvider)
 
 local Props = t.interface({
 	message = Types.IMessage
@@ -14,68 +15,95 @@ local Props = t.interface({
 
 local PADDING = 8
 
+local function getScaleFromDistance(distance)
+	-- (distance, scale) pairs
+	local d1, s1 = 20, 1.2
+	local d2, s2 = 80, 0.05
+
+	local slope = (s2-s1)/(d2-d1)
+
+	-- slope-point form
+	local scale = slope*(distance-d1) + s1
+
+	return math.clamp(scale, s2, s1)
+end
+
 local function MessageBillboard(props)
 	assert(Props(props))
 
 	local messagePart = Messages.getMessagePartById(props.message.id)
 
-	return Roact.createElement("BillboardGui", {
-		MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
-		Size = UDim2.new(0, 450, 0, 200),
-		LightInfluence = 0,
-		Adornee = messagePart
-	}, {
-		Container = Roact.createElement("Frame", {
-			BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-			Size = UDim2.new(1, 0, 1, 0),
-			BorderSizePixel = 0
-		}, {
-			Padding = Roact.createElement(Padding, {
-				size = PADDING
-			}),
-
-			Layout = Roact.createElement("UIListLayout", {
-				SortOrder = Enum.SortOrder.LayoutOrder,
-				FillDirection = Enum.FillDirection.Horizontal
-			}),
-
-			Sidebar = Roact.createElement("Frame", {
-				Size = UDim2.new(1/5, 0, 1, 0),
-				BackgroundTransparency = 1,
-				LayoutOrder = 1,
+	return Roact.createElement(CameraDistanceProvider, {
+		origin = messagePart.Position,
+		render = function(distance)
+			return Roact.createElement("BillboardGui", {
+				MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
+				Size = UDim2.new(0, 450, 0, 200),
+				LightInfluence = 0,
+				Adornee = messagePart
 			}, {
-				Avatar = Roact.createElement(Avatar, {
-					userId = props.message.authorId,
-				})
-			}),
+				Container = Roact.createElement("Frame", {
+					BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+					Size = UDim2.new(1, 0, 1, 0),
+					BorderSizePixel = 0,
 
-			Main = Roact.createElement("Frame", {
-				Size = UDim2.new(4/5, -PADDING, 1, 0),
-				BackgroundTransparency = 1,
-				LayoutOrder = 2,
-			}, {
-				Layout = Roact.createElement("UIListLayout", {
-					SortOrder = Enum.SortOrder.LayoutOrder,
-					Padding = UDim.new(0, 8)
-				}),
+					-- This makes it so when the scale is changed, the billboard remains
+					-- at the center.
+					AnchorPoint = Vector2.new(.5, .5),
+					Position = UDim2.new(.5, 0, .5, 0),
+				}, {
+					Scale = Roact.createElement("UIScale", {
+						Scale = getScaleFromDistance(distance)
+					}),
 
-				Padding = Roact.createElement("UIPadding", {
-					PaddingLeft = UDim.new(0, PADDING)
-				}),
+					Padding = Roact.createElement(Padding, {
+						size = PADDING
+					}),
 
-				Meta = Roact.createElement(MessageMeta, {
-					message = props.message,
-					size = UDim2.new(1, 0, 0, 22),
-					layoutOrder = 1,
-				}),
+					Layout = Roact.createElement("UIListLayout", {
+						SortOrder = Enum.SortOrder.LayoutOrder,
+						FillDirection = Enum.FillDirection.Horizontal
+					}),
 
-				Body = Roact.createElement(MessageBody, {
-					message = props.message,
-					size = UDim2.new(1, 0, 0.85, 0),
-					layoutOrder = 2,
+					Sidebar = Roact.createElement("Frame", {
+						Size = UDim2.new(1/5, 0, 1, 0),
+						BackgroundTransparency = 1,
+						LayoutOrder = 1,
+					}, {
+						Avatar = Roact.createElement(Avatar, {
+							userId = props.message.authorId,
+						})
+					}),
+
+					Main = Roact.createElement("Frame", {
+						Size = UDim2.new(4/5, -PADDING, 1, 0),
+						BackgroundTransparency = 1,
+						LayoutOrder = 2,
+					}, {
+						Layout = Roact.createElement("UIListLayout", {
+							SortOrder = Enum.SortOrder.LayoutOrder,
+							Padding = UDim.new(0, 8)
+						}),
+
+						Padding = Roact.createElement("UIPadding", {
+							PaddingLeft = UDim.new(0, PADDING)
+						}),
+
+						Meta = Roact.createElement(MessageMeta, {
+							message = props.message,
+							size = UDim2.new(1, 0, 0, 22),
+							layoutOrder = 1,
+						}),
+
+						Body = Roact.createElement(MessageBody, {
+							message = props.message,
+							size = UDim2.new(1, 0, 0.85, 0),
+							layoutOrder = 2,
+						})
+					})
 				})
 			})
-		})
+		end
 	})
 end
 

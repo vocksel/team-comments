0d88b69f99a403b56ae7e7e09a2bacb7d6bbe35e
David Minnerly <voxeldavid@gmail.com>
2019-05-25 16:46:35 -0400

    Make the world messages adhere to the theme

--- src/Components/BillboardMessage.lua
+++ src/Components/BillboardMessage.lua
@@ -9,6 +9,7 @@ local MessageMeta = require(script.Parent.MessageMeta)
 local MessageBody = require(script.Parent.MessageBody)
 local Padding = require(script.Parent.Padding)
 local CameraDistanceProvider = require(script.Parent.CameraDistanceProvider)
+local StudioThemeAccessor = require(script.Parent.StudioThemeAccessor)
 
 local Props = t.interface({
 	message = Types.IMessage
@@ -30,78 +31,82 @@ end
 local function BillboardMessage(props)
 	assert(Props(props))
 
-	local messagePart = Messages.getMessagePartById(props.message.id)
+	return StudioThemeAccessor.withTheme(function(theme)
+		local messagePart = Messages.getMessagePartById(props.message.id)
 
-	return Roact.createElement(CameraDistanceProvider, {
-		origin = messagePart.Position,
-		render = function(distance)
-			return Roact.createElement("BillboardGui", {
-				MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
-				Size = UDim2.new(0, 450, 0, 200),
-				LightInfluence = 0,
-				Adornee = messagePart
-			}, {
-				Container = Roact.createElement("Frame", {
-					BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-					Size = UDim2.new(1, 0, 1, 0),
-					BorderSizePixel = 0,
-
-					-- This makes it so when the scale is changed, the billboard remains
-					-- at the center.
-					AnchorPoint = Vector2.new(.5, .5),
-					Position = UDim2.new(.5, 0, .5, 0),
+		return Roact.createElement(CameraDistanceProvider, {
+			origin = messagePart.Position,
+			render = function(distance)
+				return Roact.createElement("BillboardGui", {
+					MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
+					Size = UDim2.new(0, 450, 0, 200),
+					LightInfluence = 0,
+					Adornee = messagePart
 				}, {
-					Scale = Roact.createElement("UIScale", {
-						Scale = getScaleFromDistance(distance)
-					}),
+					Container = Roact.createElement("Frame", {
+						BackgroundColor3 = theme:GetColor("MainBackground"),
+						Size = UDim2.new(1, 0, 1, 0),
+						BorderSizePixel = 0,
 
-					Padding = Roact.createElement(Padding),
-
-					Layout = Roact.createElement("UIListLayout", {
-						SortOrder = Enum.SortOrder.LayoutOrder,
-						FillDirection = Enum.FillDirection.Horizontal
-					}),
-
-					Sidebar = Roact.createElement("Frame", {
-						Size = UDim2.new(1/5, 0, 1, 0),
-						BackgroundTransparency = 1,
-						LayoutOrder = 1,
+						-- This makes it so when the scale is changed, the billboard remains
+						-- at the center.
+						AnchorPoint = Vector2.new(.5, .5),
+						Position = UDim2.new(.5, 0, .5, 0),
 					}, {
-						Avatar = Roact.createElement(Avatar, {
-							userId = props.message.authorId,
-						})
-					}),
+						Scale = Roact.createElement("UIScale", {
+							Scale = getScaleFromDistance(distance)
+						}),
 
-					Main = Roact.createElement("Frame", {
-						Size = UDim2.new(4/5, -Styles.Padding, 1, 0),
-						BackgroundTransparency = 1,
-						LayoutOrder = 2,
-					}, {
+						Padding = Roact.createElement(Padding),
+
 						Layout = Roact.createElement("UIListLayout", {
 							SortOrder = Enum.SortOrder.LayoutOrder,
-							Padding = UDim.new(0, Styles.Padding)
+							FillDirection = Enum.FillDirection.Horizontal
 						}),
 
-						Padding = Roact.createElement("UIPadding", {
-							PaddingLeft = UDim.new(0, Styles.Padding)
+						Sidebar = Roact.createElement("Frame", {
+							Size = UDim2.new(1/5, 0, 1, 0),
+							BackgroundTransparency = 1,
+							LayoutOrder = 1,
+						}, {
+							Avatar = Roact.createElement(Avatar, {
+								userId = props.message.authorId,
+								maskColor = theme:GetColor("MainBackground")
+							})
 						}),
 
-						Meta = Roact.createElement(MessageMeta, {
-							message = props.message,
-							size = UDim2.new(1, 0, 0, 22),
-							layoutOrder = 1,
-						}),
+						Main = Roact.createElement("Frame", {
+							Size = UDim2.new(4/5, -Styles.Padding, 1, 0),
+							BackgroundTransparency = 1,
+							LayoutOrder = 2,
+						}, {
+							Layout = Roact.createElement("UIListLayout", {
+								SortOrder = Enum.SortOrder.LayoutOrder,
+								Padding = UDim.new(0, Styles.Padding)
+							}),
 
-						Body = Roact.createElement(MessageBody, {
-							message = props.message,
-							size = UDim2.new(1, 0, 0.85, 0),
-							layoutOrder = 2,
+							Padding = Roact.createElement("UIPadding", {
+								PaddingLeft = UDim.new(0, Styles.Padding)
+							}),
+
+							Meta = Roact.createElement(MessageMeta, {
+								message = props.message,
+								size = UDim2.new(1, 0, 0, 22),
+								layoutOrder = 1,
+							}),
+
+							Body = Roact.createElement(MessageBody, {
+								message = props.message,
+								size = UDim2.new(1, 0, 0.85, 0),
+								layoutOrder = 2,
+							})
 						})
 					})
 				})
-			})
-		end
-	})
+			end
+		})
+
+	end)
 end
 
 return BillboardMessage

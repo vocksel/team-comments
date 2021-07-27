798593cd1f0260021481fdff0c23b6673f389dd8
David Minnerly <voxeldavid@gmail.com>
2021-07-27 18:47:56 -0400

    Fix BillboardMessage story

--- src/Components/BillboardMessage.story.lua
+++ src/Components/BillboardMessage.story.lua
@@ -1,27 +1,18 @@
 local Roact = require(script.Parent.Parent.Packages.Roact)
-local Rodux = require(script.Parent.Parent.Packages.Rodux)
-local RoactRodux = require(script.Parent.Parent.Packages.RoactRodux)
 local BillboardMessage = require(script.Parent.BillboardMessage)
-local Reducer = require(script.Parent.Parent.Reducer)
 
 return function(target)
-    local store = Rodux.Store.new(Reducer)
-
-    local root = Roact.createElement(RoactRodux.StoreProvider, {
-        store = store
+    local root = Roact.createElement("Frame", {
+        Size = UDim2.fromScale(0.5, 0.8),
+        BackgroundTransparency = 1,
     }, {
-        Wrapper = Roact.createElement("Frame", {
-            Size = UDim2.fromScale(0.5, 0.8),
-            BackgroundTransparency = 1,
-        }, {
-            BillboardMessage = Roact.createElement(BillboardMessage, {
-                message = {
-                    id = "1",
-                    userId = "1343930",
-                    body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel leo bibendum, efficitur eros vel, tincidunt est. Aenean augue velit, volutpat et posuere quis, fermentum quis purus. Etiam est risus, fringilla sit amet elementum eget, elementum eu tellus. Vestibulum vehicula nibh felis, at gravida metus rhoncus eu. Curabitur ornare sodales varius.",
-                    time = os.time(),
-                }
-            })
+        BillboardMessage = Roact.createElement(BillboardMessage, {
+            message = {
+                id = "1",
+                userId = "1343930",
+                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel leo bibendum, efficitur eros vel, tincidunt est. Aenean augue velit, volutpat et posuere quis, fermentum quis purus. Etiam est risus, fringilla sit amet elementum eget, elementum eu tellus. Vestibulum vehicula nibh felis, at gravida metus rhoncus eu. Curabitur ornare sodales varius.",
+                createdAt = os.time(),
+            }
         })
     })
 

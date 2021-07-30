55bb0017d1bd11b1d33f0d157fe3d117b6a7e367
David Minnerly <voxeldavid@gmail.com>
2021-07-30 19:18:01 -0400

    Clean up BillboardApp

--- src/Components/BillboardApp.lua
+++ src/Components/BillboardApp.lua
@@ -2,7 +2,6 @@ local Roact = require(script.Parent.Parent.Packages.Roact)
 local Comment = require(script.Parent.Comment)
 local Config = require(script.Parent.Parent.Config)
 local Messages = require(script.Parent.Parent.Messages)
-local CameraDistanceProvider = require(script.Parent.CameraDistanceProvider)
 local MessageContext = require(script.Parent.MessageContext)
 
 local function BillboardApp()
@@ -14,21 +13,16 @@ local function BillboardApp()
             for _, message in pairs(context.messages) do
                 local messagePart = Messages.getMessagePartById(message.id)
 
-                children[message.id] = Roact.createElement(CameraDistanceProvider, {
-                    origin = messagePart.Position,
-                    render = function(distance)
-                        return Roact.createElement("BillboardGui", {
-                            MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
-                            Size = UDim2.new(0, 450, 0, 200),
-                            LightInfluence = 0,
-                            Adornee = messagePart
-                        }, {
-                            Roact.createElement(Comment, {
-                                message = message,
-                                distance = distance,
-                            })
-                        })
-                    end
+                children[message.id] = Roact.createElement("BillboardGui", {
+                    MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
+                    Size = UDim2.new(0, 450, 0, 200),
+                    LightInfluence = 0,
+                    Adornee = messagePart,
+                    Active = true,
+                }, {
+                    Comment = Roact.createElement(Comment, {
+                        message = message,
+                    })
                 })
             end
 

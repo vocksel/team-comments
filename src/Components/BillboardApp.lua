94b16d0c1f43c6620bc7f37d7f5c4f381c281e03
David Minnerly <voxeldavid@gmail.com>
2021-07-30 22:04:36 -0400

    Make a componnet for the billboard

--- src/Components/BillboardApp.lua
+++ src/Components/BillboardApp.lua
@@ -1,8 +1,7 @@
 local Roact = require(script.Parent.Parent.Packages.Roact)
-local Comment = require(script.Parent.Comment)
-local Config = require(script.Parent.Parent.Config)
 local Messages = require(script.Parent.Parent.Messages)
 local MessageContext = require(script.Parent.MessageContext)
+local CommentBillboard = require(script.Parent.CommentBillboard)
 
 local function BillboardApp()
     -- if props.ui.areMessagesVisible then
@@ -11,18 +10,9 @@ local function BillboardApp()
             local children = {}
 
             for _, message in pairs(context.messages) do
-                local messagePart = Messages.getMessagePartById(message.id)
-
-                children[message.id] = Roact.createElement("BillboardGui", {
-                    MaxDistance = Config.BILLBOARD_MAX_DISTANCE,
-                    Size = UDim2.new(0, 450, 0, 200),
-                    LightInfluence = 0,
-                    Adornee = messagePart,
-                    Active = true,
-                }, {
-                    Comment = Roact.createElement(Comment, {
-                        message = message,
-                    })
+                children[message.id] = Roact.createElement(CommentBillboard, {
+                    messagePart = Messages.getMessagePartById(message.id),
+                    message = message,
                 })
             end
 
---
+++ src/Components/CommentBillboard.lua
@@ -0,0 +1,49 @@
+local Roact = require(script.Parent.Parent.Packages.Roact)
+local t = require(script.Parent.Parent.Packages.t)
+local types = require(script.Parent.Parent.Types)
+local CameraDistanceProvider = require(script.Parent.CameraDistanceProvider)
+local Comment = require(script.Parent.Comment)
+
+local CommentBillboard = Roact.Component:extend("CommentBillboard")
+
+CommentBillboard.validateProps = t.interface({
+    messagePart = t.instance("Part"),
+    message = types.IMessage
+})
+
+function CommentBillboard:init()
+    self.onActivated = function()
+        print("focus")
+    end
+end
+
+function CommentBillboard:render()
+    return Roact.createElement(CameraDistanceProvider, {
+        origin = self.props.messagePart.Position,
+        render = function(distance)
+            return Roact.createElement("BillboardGui", {
+                MaxDistance = math.huge,
+                Size = UDim2.fromOffset(400, 200),
+                LightInfluence = 0,
+                Adornee = self.props.messagePart,
+                Active = true,
+            }, {
+                Layout = Roact.createElement("UIListLayout", {
+                    HorizontalAlignment = Enum.HorizontalAlignment.Center,
+                    VerticalAlignment = Enum.VerticalAlignment.Center,
+                }),
+
+                Comment = distance <= 100 and Roact.createElement(Comment, {
+                    message = self.props.message,
+                }),
+
+                Bubble = distance > 100 and Roact.createElement("ImageButton", {
+                    Size = UDim2.fromOffset(64, 64),
+                    [Roact.Event.Activated] = self.onActivated
+                })
+            })
+        end
+    })
+end
+
+return CommentBillboard

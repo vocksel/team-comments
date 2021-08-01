8c7bd49c30aa4353cc5916ed35605e4b01889f86
David Minnerly <voxeldavid@gmail.com>
2021-08-01 18:12:17 -0400

    Add focusing message selection from comment bubbles

--- src/Components/BillboardApp.lua
+++ src/Components/BillboardApp.lua
@@ -2,24 +2,32 @@ local TeamComments = script:FindFirstAncestor("TeamComments")
 
 local Roact = require(TeamComments.Packages.Roact)
 local Hooks = require(TeamComments.Packages.Hooks)
+local t = require(TeamComments.Packages.t)
 local MessageContext = require(TeamComments.Context.MessageContext)
 local useCameraDistance = require(TeamComments.Hooks.useCameraDistance)
 local CommentBubble = require(script.Parent.CommentBubble)
 
-local function BillboardApp(_props, hooks)
-	local messages = hooks.useContext(MessageContext)
+local validateProps = t.interface({
+	widget = t.instance("DockWidgetPluginGui"),
+})
 
+local function BillboardApp(props, hooks)
+	assert(validateProps(props))
+
+	local messages = hooks.useContext(MessageContext)
 	local children = {}
 
 	for _, message in pairs(messages.getComments()) do
 		local messagePart = messages.getAdornee(message.id)
 
+		local function onActivated()
+			props.widget.Enabled = true
+			messages.focusAdornee(message.id)
+			messages.setSelectedMessage(message.id)
+		end
+
 		if messagePart then
 			local distance = useCameraDistance(hooks, messagePart.Position)
-
-			local function onActivated()
-				print("activated")
-			end
 
 			children[message.id] = Roact.createElement("BillboardGui", {
 				MaxDistance = math.huge,

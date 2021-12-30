99ca525503d70753111f919e2e93ef1a39d69a78
David Minnerly <me@vocksel.com>
2021-12-30 08:15:41 -0800

    Tidy up the main script

---
+++ src/Plugin/createToggleButton.lua
@@ -0,0 +1,33 @@
+local config = require(script.Parent.Parent.config)
+local assets = require(script.Parent.Parent.assets)
+
+--[=[
+    Creates the button to toggle the plugin widget.
+
+    This function also sets up some events to toggle the widget when the button
+    is clicked, and to sync up the button's "active" state with the widget.
+
+    @return () -> nil -- Returns a callback for disconnecting button events
+]=]
+local function createToggleButton(toolbar: PluginToolbar, widget: DockWidgetPluginGui)
+	local toggleAppView = toolbar:CreateButton(
+		config.DISPLAY_NAME,
+		"View and edit the list of messages",
+		assets.CommentBubble
+	)
+
+	local click = toggleAppView.Click:Connect(function()
+		widget.Enabled = not widget.Enabled
+	end)
+
+	local enabled = widget:GetPropertyChangedSignal("Enabled"):Connect(function()
+		toggleAppView:SetActive(widget.Enabled)
+	end)
+
+	return function()
+		click:Disconnect()
+		enabled:Disconnect()
+	end
+end
+
+return createToggleButton
--- src/main.server.lua
+++ src/main.server.lua
@@ -1,21 +1,3 @@
--- Button
--- When pressed, prompt the user with a textbox
---		Type inside, press enter to finish
---		Unfocusing the textbox destroys it
--- Creates a new free floating billboard with the text
---		Will also maintain metadata of who wrote the message, the date/time, and a little avatar.
-
--- List of all notes
---		Clicking a note jumps to it in the game
---		Let author edit
---		Allow deleting notes
-
--- Have all TeamComments stored as Parts in the workspace
--- When Studio boots up, create state out of all the TeamComments' Value instances
--- 	When a new "TeamComment" tagged Part is added, add a new one
--- 	When a TeamComment tagged part is destroyed, remove it from the state
--- 	When a TeamComment is removed from the state, remove it from the game as well (link via IDs)
-
 local CoreGui = game:GetService("CoreGui")
 
 local Roact = require(script.Parent.Packages.Roact)
@@ -23,51 +5,17 @@ local PluginApp = require(script.Parent.Components.PluginApp)
 local BillboardApp = require(script.Parent.Components.BillboardApp)
 local MessageContext = require(script.Parent.Context.MessageContext)
 local config = require(script.Parent.config)
-local assets = require(script.Parent.assets)
+local createWidget = require(script.Parent.Plugin.createWidget)
+local createToggleButton = require(script.Parent.Plugin.createToggleButton)
 
 local toolbar = plugin:CreateToolbar(config.DISPLAY_NAME)
-
-local function createWidget()
-	local info = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Left, true)
-	local widgetName = config.PLUGIN_NAME .. "App"
-	local widget = plugin:CreateDockWidgetPluginGui(widgetName, info)
-
-	widget.Name = widgetName
-	widget.Title = config.DISPLAY_NAME
-	widget.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-	return widget
-end
-
-local function createButtons(widget)
-	local toggleAppView = toolbar:CreateButton(
-		config.DISPLAY_NAME,
-		"View and edit the list of messages",
-		assets.CommentBubble
-	)
-
-	toggleAppView.Click:Connect(function()
-		widget.Enabled = not widget.Enabled
-	end)
-
-	widget:GetPropertyChangedSignal("Enabled"):Connect(function()
-		toggleAppView:SetActive(widget.Enabled)
-	end)
-end
-
-local widget = createWidget()
-createButtons(widget)
-
--- TODO: Use plugin:SetSetting() and plugin:GetSetting() to keep track of which
--- comments have new activity for the client. This will allow us to add badges
--- to notify the client of new activity on comments. Hopefully we can store
--- tables with this approach.
+local widget = createWidget(plugin)
+local disconnectButtonEvents = createToggleButton(toolbar, widget)
 
 local ui = Roact.createElement(MessageContext.Provider, {
 	messageTag = "TeamComment",
 }, {
 	PluginApp = Roact.createElement(PluginApp, {
-		widget = widget,
 		-- selene: allow(incorrect_standard_library_use)
 		userId = tostring(plugin:GetStudioUserId()),
 	}),
@@ -86,7 +34,7 @@ local ui = Roact.createElement(MessageContext.Provider, {
 	}),
 })
 
-Roact.setGlobalconfig({
+Roact.setGlobalConfig({
 	typeChecks = true,
 	elementTracing = true,
 	propValidation = true,
@@ -96,4 +44,5 @@ local handle = Roact.mount(ui, widget, "Apps")
 
 plugin.Unloading:Connect(function()
 	Roact.unmount(handle)
+	disconnectButtonEvents()
 end)

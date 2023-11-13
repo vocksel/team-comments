51d4c7d759742bc06609f0cf565fb53101db2788
Marin Minnerly <me@vocksel.com>
2023-11-12 18:42:12 -0800

    Fix PluginApp story

--- src/Components/PluginApp.story.lua
+++ src/Components/PluginApp.story.lua
@@ -6,7 +6,7 @@ local MessageContext = require(TeamComments.Context.MessageContext)
 
 return {
     story = function()
-        Roact.createElement(MessageContext.Provider, { messageTag = "TeamComment" }, {
+        return Roact.createElement(MessageContext.Provider, { messageTag = "TeamComment" }, {
             Wrapper = Roact.createElement("Frame", {
                 Size = UDim2.new(0, 400, 1, 0),
                 BackgroundTransparency = 1,
